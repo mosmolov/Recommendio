f2f6a5f643761e7e1e7647dff04130ccffb4dd01
abhinavish <136651268+abhinavish@users.noreply.github.com>
2023-10-05 01:02:32 -0400

    Update main.py

--- main.py
+++ main.py
@@ -2,3 +2,19 @@ import streamlit as st
 
 '### This is our Project for CS4641: Machine Learning'
 'More to Follow...'
+
+'A project proposal should include:'
+
+'Introduction/Background: A quick introduction of your topic and mostly literature review of what has been done in this area.' 
+'You can briefly explain your dataset and its features here too.'
+
+'Problem definition: Why there is a problem here or what is the motivation of the project?'
+
+'Methods: What algorithms or methods are you going to use to solve the problems. (Note: Use existing packages/libraries)'
+
+'Potential results and Discussion: Discuss about what type of quantitative metrics your team plan to use for the project (i.e. ML Metrics).'
+
+'At least three references (preferably peer reviewed). You need to properly cite the references on your proposal. This part does NOT count towards word limit.'
+'Add proposed timeline from start to finish and list each project members’ responsibilities. Fall and Spring semester sample Gantt Chart. This part does NOT count towards word limit.'
+'A contribution table with all group members’ names that explicitly provides the contribution of each member in preparing the project task. This part does NOT count towards word limit.'
+'A checkpoint to make sure you are working on a proper machine learning related project. You are required to have your dataset ready when you submit your proposal. You can change dataset later. However, you are required to provide some reasonings why you need to change the dataset (i.e. dataset is not large enough because it does not provide us a good accuracy comparing to other dataset; we provided accuracy comparison between these two datasets). The reasonings can be added as a section to your future project reports such as midterm report.'
