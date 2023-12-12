602c6c01f00cccd68c5e44c2b07353150f77cb76
Michael Osmolovskiy <mosmolovskiy@gmail.com>
2023-12-12 12:19:20 -0500

    fix

--- models.py
+++ models.py
@@ -1,3 +1,4 @@
+import sklearn
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import classification_report, accuracy_score
